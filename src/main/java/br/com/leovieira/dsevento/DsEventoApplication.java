fd4c7859683e5dd93546dbbf59b2a84c12e475a8
leovieira84 <leonardo.vieira@gmail.com>
2024-11-16 11:10:03 -0300

    Configuracao inicial do projeto.

---
+++ src/main/java/br/com/leovieira/dsevento/DsEventoApplication.java
@@ -0,0 +1,13 @@
+package br.com.leovieira.dsevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leovieira/dsevento/DsEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leovieira.dsevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
