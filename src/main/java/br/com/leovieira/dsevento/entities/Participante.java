db0a80b45f4afaed1ee49feb70fe9d9314778fde
leovieira84 <leonardo.vieira@gmail.com>
2024-11-16 12:40:30 -0300

    Alteração do atributo email (Participante) para ser unico

--- src/main/java/br/com/leovieira/dsevento/entities/Participante.java
+++ src/main/java/br/com/leovieira/dsevento/entities/Participante.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,7 @@ public class Participante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	@Column(unique = true)
 	private String email;
 	
 	@ManyToMany
